e111bffdde5a2fac2962959f58c3c872e048796b
Maarten Mäcking <maarten.maecking@studium.uni-hamburg.de>
2022-12-05 00:06:42 +0100

    Add uploaded files directly into the Molecule compounds model

--- Uploader/views.py
+++ Uploader/views.py
@@ -1,71 +1,69 @@
+import os.path
 from django.shortcuts import render
-from .forms import SDFileForm,SDFileMult
-from rdkit import Chem
+from .forms import SDFileForm, SDFileMult
 from dashboard.settings import MEDIA_ROOT
-from compounds.models import Molecule
+from compounds.calc_mol_properties import MoleculeIterator
+
 
 def SDFile_Upload(request):
-    form=SDFileForm()
-    data={'form':form}
+    form = SDFileForm()
+    data = {'form': form}
     if request.method == 'POST':
-        form=SDFileForm(request.POST,request.FILES)
+        form = SDFileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            
+            file = form['document'].value()
+            file = str(file)
             if file.endswith('.sdf'):
-                files=request.FILES.getlist('document')
-                file_path = MEDIA_ROOT + '/uploaded_data/'
-                with Chem.SDMolSupplier(file_path+file) as suppl:
-                    for substance in suppl:
-                        if substance is None:
-                            continue
-                            subst = Molecule()
-                            subst.inchi_key = substance.GetProp('PUBCHEM_IUPAC_INCHIKEY')
-                            subst.log_p = substance.GetProp('PUBCHEM_XLOGP3')
-                            subst.num_h_acceptors = substance.GetProp('PUBCHEM_CACTVS_HBOND_ACCEPTOR')
-                            subst.num_h_donors = substance.GetProp('PUBCHEM_CACTVS_HBOND_DONOR')
-                            subst.molecular_mass = substance.GetProp('PUBCHEM_MOLECULAR_WEIGHT')
-                            subst.save()
-                data['form']=form
-                data['existing_uploads']={'headers':['File','Set','Description']}
-                data['existing_uploads']['data'] = [[file,1,form['description'].value()]]
-            else:
-                err_msg='Not a SDFile'
-                data['err_msg']=err_msg
-    return render(request,'upload.html',data)
+                file_path = os.path.join(MEDIA_ROOT, 'uploaded_data/', file)
+                try:
+                    m = MoleculeIterator(file_path)
+                    m.iterate_over_molecules()
+                    data['form'] = form
+                    data['existing_uploads'] = {
+                        'headers': ['File', 'Set', 'Description']}
+                    data['existing_uploads']['data'] = [
+                        [file, 1, form['description'].value()]]
+                except ValueError:
+                    data['err_msg'] = 'Not a valid SDFile'
 
-def save_file(file):
-    with open(MEDIA_ROOT + '/uploaded_data/' + file.name,'wb+') as destination:
+            else:
+                err_msg = 'Not a SDFile'
+                data['err_msg'] = err_msg
+    return render(request, 'upload.html', data)
+
+
+def save_file(file, file_path):
+    with open(file_path, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
 
+
 def SDFMultView(request):
-    form=SDFileMult()
-    data={'form':form}
+    form = SDFileMult()
+    data = {'form': form}
+
     if request.method == 'POST':
-        form=SDFileMult(request.POST,request.FILES)
+        form = SDFileMult(request.POST, request.FILES)
         if form.is_valid():
-            data['existing_uploads']={'headers':['File','Set','Description'],'data':[]}
+            data['existing_uploads'] = \
+                {'headers': ['File', 'Set', 'Description'], 'data': []}
+
             for file in request.FILES.getlist('documents'):
-                save_file(file)
-                file=str(file)
-                print(file)
-                if str(file).endswith('.sdf'):
-                    file_path = MEDIA_ROOT + '/uploaded_data/'
-                    with Chem.SDMolSupplier(file_path+file) as suppl:
-                        for substance in suppl:
-                            if substance is None:
-                                continue
-                            '''subst = Molecule()
-                            subst.inchi_key = substance.GetProp('PUBCHEM_IUPAC_INCHIKEY')
-                            subst.log_p = substance.GetProp('PUBCHEM_XLOGP3')
-                            subst.num_h_acceptors = substance.GetProp('PUBCHEM_CACTVS_HBOND_ACCEPTOR')
-                            subst.num_h_donors = substance.GetProp('PUBCHEM_CACTVS_HBOND_DONOR')
-                            subst.molecular_mass = substance.GetProp('PUBCHEM_MOLECULAR_WEIGHT')
-                            subst.save()'''
-                    data['form']=form
-                    data['existing_uploads']['data'].append([file,1,form['description'].value()])
+                file_path = os.path.join(
+                    MEDIA_ROOT, 'uploaded_data/', file.name)
+                save_file(file, file_path)
+                try:
+                    m = MoleculeIterator(file_path)
+                    m.iterate_over_molecules()
+                except ValueError:
+                    data['err_msg'] = 'Not a valid SDFile'
+                    continue
+
+                data['form'] = form
+                data['existing_uploads']['data'].append(
+                    [file, 1, form['description'].value()])
         else:
-            err_msg='Not a SDFile'
-            data['err_msg']=err_msg
-    return render(request,'upload.html',data)
+            err_msg = 'Not a valid SDFile'
+            data['err_msg'] = err_msg
+    return render(request, 'upload.html', data)
