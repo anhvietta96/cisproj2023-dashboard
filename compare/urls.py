9f7103bfd5691112203e3be0514db84a1f7344bf
Jocelyne <jocelyne@Jocelynes-MacBook-Air.local>
2022-12-07 22:44:08 +0100

    first commit

--- compare/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from . import views
-
-app_name = 'compare'
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.main_compound_view, name='compare')
-]
