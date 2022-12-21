620fe161cb8c4537f8117bf31f958a2c4e804128
Quiet <anhviet312123@gmail.com>
2022-12-21 10:02:14 +0100

    Chart functionalities added

--- chart/views.py
+++ chart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from compounds.models import Molecule
+from compounds.models import Molecule,MoleculeSet
 '''from .models import PropertyChoice
 from .forms import PropertyForm'''
 import json
@@ -11,9 +11,14 @@ class Chart:
 # Create your views here.
 def ChartOptions(request):
     data = {}
+    
     properties = Molecule.__dict__["__doc__"]
     property_list = properties[9:].replace(',','').replace(')','').split()
     data['property_list']=property_list
+
+    all_set = MoleculeSet.objects.all()
+    set_list=[str(set) for set in all_set]
+    data['set_list']=set_list
     return render(request,'chart_options.html',data)
 '''
 def ChartResult(request):
@@ -43,34 +48,50 @@ def ChartResult(request):
     return render(request,'chart_result.html',result)
 '''
 def ChartResult(request):
-    
-    x_axis = int(request.POST.get('x-axis')[0])-1
-    y_axis = int(request.POST.get('y-axis')[0])-1
-    q = Molecule.objects.all()
-    
+    ChartOptions = {'legend':[],'name':[],'data':[],'image':[]}
+    media_url = 'media/image'
+
     properties = Molecule.__dict__["__doc__"]
     property_list = properties[9:].replace(',','').replace(')','').split()
 
-    ChartOptions = {}
+    post = request.POST
 
-    data = []
-    for mol in q:
-        mol_inf = []
-        mol_inf.append(getattr(mol,property_list[x_axis]))
-        mol_inf.append(getattr(mol,property_list[y_axis]))
-        for property in property_list:
-            if property != property_list[x_axis] and property != property_list[y_axis] and property != 'image':
-                mol_inf.append(getattr(mol,property))
-        data.append(mol_inf)
+    x_axis = int(post.get('x-axis')[0])-1
+    y_axis = int(post.get('y-axis')[0])-1
 
-    ChartOptions['legend']=['Group 1']
+    all_set = MoleculeSet.objects.all()
+    set_list=[str(set) for set in all_set]
+    
+    post_dict = post.dict()
+    queried_sets = []
+    
+
+    for key in post_dict.keys():
+        if key.startswith('set_'):
+            data = []
+            set_num = int(key.split('_')[1])
+            set = all_set[set_num]
+            ChartOptions['legend'].append(set_list[set_num])
+            ChartOptions['name'].append(set_list[set_num])
+            q = set.molecules.all()
+            img_list = []
+            for mol in q:
+                mol_inf = []
+                mol_inf.append(getattr(mol,property_list[x_axis]))
+                mol_inf.append(getattr(mol,property_list[y_axis]))
+                for property in property_list:
+                    if property != property_list[x_axis] and property != property_list[y_axis] and property != 'image':
+                        mol_inf.append(getattr(mol,property))
+                    if property == 'image':
+                        img_list.append(mol.image.url)
+                data.append(mol_inf)
+            ChartOptions['data'].append(data)
+            ChartOptions['image'].append(img_list)
+
     ChartOptions['header']=[property_list[x_axis],property_list[y_axis]]
     for property in property_list:
-        if property != property_list[x_axis] and property != property_list[y_axis] and properties != 'image':
+        if property != property_list[x_axis] and property != property_list[y_axis] and property != 'image':
             ChartOptions['header'].append(property)
-    ChartOptions['name']=['Group 1']
-    ChartOptions["data"]=[data]
-    ChartOptions['title']=property_list[x_axis]+"/"+property_list[y_axis]
-    result = {'title':'Created Chart','options':ChartOptions}
     
+    result = {'title':property_list[x_axis]+"/"+property_list[y_axis],'options':ChartOptions}
     return render(request,'chart_result.html',result)
--- static/js/chart.js
+++ static/js/chart.js
@@ -11,18 +11,22 @@ if(dynamicchart_bool){
 
 function numerical_filter(data,pos,lower_bound,upper_bound)
 {
-  console.log(data);
-  for(let i = 0;i<data.length;i++)
+  
+  var filtered_data = [];
+  for(const i of Object.keys(data))
   {
+    /*data[i] = data[i].filter(function(subarray){return subarray[pos] > lower_bound && subarray[pos] < upper_bound})*/
+    var sub_arr = [];
     for(let j = 0;j<data[i].length;j++)
     {
-      if(data[i][j][pos]<lower_bound || data[i][j][pos]>upper_bound)
+      if(data[i][j][pos]>lower_bound && data[i][j][pos]<upper_bound)
       {
-        delete data[i][j];
+        sub_arr.push(data[i][j]);
       }
     }
+    filtered_data.push(sub_arr);
   }
-  return;
+  return filtered_data;
 }
 
 function lexicographic_filter(data,pos,substring)
@@ -41,77 +45,115 @@ function lexicographic_filter(data,pos,substring)
 }
 
 /*Echarts Lib, functional*/
-var ChartData = null;
+var ChartOptions = null;
 var Chart = null;
 var option;
 
 var chartDom = document.getElementById('dynamic-chart');
 if(chartDom) {
-  Chart = echarts.init(chartDom,null,{width:900, height:450});
-  ChartData = JSON.parse(document.currentScript.nextElementSibling.textContent);
-  var tooltip_col = ChartData['header'];
+  Chart = echarts.init(chartDom,null,{width:1500, height:1260});
+  ChartOptions = JSON.parse(document.currentScript.nextElementSibling.textContent);
+  var tooltip_col = ChartOptions['header'];
+  var chart_data = ChartOptions['data'];
+  
+  var x_values = [];
+  var y_values = [];
+  var x_col = [];
+  var y_col = []
+  for(let i = 0; i < chart_data.length;i++)
+  {
+    x_col = chart_data[i].map(function(value,index){return value[0];});
+    x_values.push.apply(x_values,x_col);
+    y_col = chart_data[i].map(function(value,index){return value[1];});
+    y_values.push.apply(y_values,y_col);
+  }
+  
+  var x_min = Math.min.apply(null,x_values);
+  var x_max = Math.max.apply(null,x_values);
+  var y_min = Math.min.apply(null,y_values);
+  var y_max = Math.max.apply(null,y_values);
+  var series_val = [];
+  var series = null;
+  var rich_text = `{"Attr":{"width": 60,"align":"left","padding":[0,10,0,10]},"Val":{"width": 90,"align":"right","padding":[0,10,0,10]},`;
+  var image_container = null;
+  var inchikey = null;
+  for(let i = 0;i<chart_data.length;i++)
+  {
+    image_container=[];
+    image_url_collection = ChartOptions['image'][i];
+    for(let j = 0;j<image_url_collection.length;j++)
+    {
+      image_container.push(null);
+      image_container[j]=new Image();
+      image_container[j].src=image_url_collection[j];
+      inchikey = chart_data[i][j][2];
+      rich_text += `"Image-${inchikey}":{"height":50,"backgroundColor":{"image":"null"}},`
+    }
+    rich_text = rich_text.slice(0,-1) + '}';
+    rich_text = JSON.parse(rich_text);
+    for(let j = 0;j<image_url_collection.length;j++)
+    {
+      inchikey = chart_data[i][j][2];
+      rich_text[`Image-${inchikey}`]["backgroundColor"]["image"]=image_container[j];
+    }
+    console.log(rich_text);
+    
+
+    series = {
+      name: ChartOptions['name'][i],
+      data: chart_data[i],
+      type: 'scatter',
+      symbolSize: 10,
+      emphasis: {
+        focus: 'self',
+        label: {
+          show: true,
+          formatter: function (param) {
+            return_list = [`{Image-${param.data[2]}|}`]
+            for(let i = 0; i < tooltip_col.length; i++)
+            {
+              return_list.push(`{Attr|${tooltip_col[i]}}{Val|${param.data[i]}}`);
+            }
+            var rtext = return_list.join('\n')
+            console.log(rtext);
+            return rtext;
+          },
+          position: 'top',
+          color: '#000',
+          backgroundColor: '#fff',
+          fontWeight: 'bold',
+          padding: [3,4],
+          width: 300,
+          height: 100,
+          fontSize: 10,
+          align: 'center',
+          verticalAlign: 'bottom',
+          rich:rich_text,
+        }
+      },
+    }
+    series_val.push(series);
+    rich_text=`{"Attr":{"width": 60,"align":"left","padding":[0,10,0,10]},"Val":{"width": 90,"align":"right","padding":[0,10,0,10]},`
+  }
+  
   option={
-    title: {
-      text: ChartData['title'],
-      left: '0%',
-      top: '0%'
-    },
     legend: {
       right: '10%',
       top: '3%',
-      data: ChartData['legend'],
+      data: ChartOptions['legend'],
     },
-    xAxis: {
-      min: 0,
-      max: 8,},
+    grid: {
+      left: '10%',
+      right: '10%',
+      top: 0,
+      bottom: 0
+      },xAxis: {
+      min: x_min-0.1,
+      max: x_max+0.1,},
     yAxis: {
-      min: -0.1,
-      max: 8.1,},
-    series: [
-        {
-          name: ChartData['name'][0],
-          data: ChartData['data'][0],
-          type: 'scatter',
-          symbolSize: 10,
-          emphasis: {
-            focus: 'self',
-            label: {
-              show: true,
-              formatter: function (param) {
-                return_list = []
-                for(let i = 0; i < tooltip_col.length; i++)
-                {
-                  return_list.push(`{Attr|${tooltip_col[i]}}{Val|${param.data[i]}}`);
-                }
-                return return_list.join('\n');
-              },
-              position: 'top',
-              color: '#000',
-              backgroundColor: '#fff',
-              fontWeight: 'bold',
-              padding: [3,4],
-              width: 300,
-              height: 100,
-              fontSize: 10,
-              align: 'center',
-              verticalAlign: 'bottom',
-              rich:{
-                Attr:{
-                    width: 60,
-                    align:'left',
-                    padding:[0,10,0,10],
-                  },
-                Val:{
-                  width: 90,
-                  align:'right',
-                  padding:[0,10,0,10],
-                }
-                
-              }
-            }
-          },
-        },
-      ]
+      min: y_min-0.1,
+      max: y_max+0.1,},
+    series: series_val,
   };
   option && Chart.setOption(option);
 }
@@ -120,38 +162,40 @@ $(document).ready(function(){
   var num_filter = 0;
   var available_filter = []
   $('#add-filter').click(function(){
-    var available_slot = null;
+    var index = null;
     for(let i = 0; i <= num_filter;i++)
     {
       if(!available_filter.includes(i))
       {
-        available_slot = i;
+        index = i;
         break;
       }
     }
-    console.log(available_slot);
     var new_div = document.createElement('div');
-    new_div.id='filter_div_'+available_slot;
+    new_div.id='filter_div_'+index;
     var new_select = document.createElement('select');
-    new_select.id='filter_'+available_slot;
-    var header = ChartData['header'];
+    new_select.id='filter_'+index;
+    var header = ChartOptions['header'];
     var html = '';
     for(let i = 0;i<header.length;i++)
     {
       html += `<option value=${i}>${header[i]}</option>`;
     }
+    var html_a="<a id='dynamic_input_" + index + "'>";
+    html_a += ` ranges from <input type=text id=filter_input_${index}_0></input> to <input type=text id=filter_input_${index}_1></input>`;
+
     var button = document.createElement('button');
-    button.id='remove_'+available_slot;
+    button.id='remove_'+index;
     button.classList.add('filter-btn');
     const element = document.getElementById('filters');
     const elem_button = document.getElementById('add-filter');
     new_div.appendChild(button);
     new_div.appendChild(new_select);
     element.insertBefore(new_div,elem_button);
-    $('#filter_'+available_slot).append(html);
+    $('#filter_'+index).append(html);
+    $('#filter_div_'+index).append(html_a);
     num_filter++;
-    available_filter.push(available_slot);
-    console.log(num_filter==available_filter.length);
+    available_filter.push(index);
     return;
   });
   $('#filters').on('change','select',function(events){
@@ -159,11 +203,11 @@ $(document).ready(function(){
     var split_id = id.split("_");
     var index = split_id[1];
     
-    if(document.getElementById('dynamic_input_'+id))
+    if(document.getElementById('dynamic_input_'+index))
     {
-      $('#dynamic_input_'+id).remove();
+      $('#dynamic_input_'+index).remove();
     }
-    var html="<a id='dynamic_input_" + id + "'>";
+    var html="<a id='dynamic_input_" + index + "'>";
     if(this.value!=2 && this.value!=4)
     {
       html+=' ranges from ';
@@ -195,30 +239,38 @@ $(document).ready(function(){
       }
     }
     available_filter=new_arr;
-    console.log(available_filter);
     return;
   });
   $('#apply-filter').click(function(){
-    var filter = null;
-    var filter_val = [null,null];
-    var curr_data = ChartData['data'];
-    for(const slot of available_filter)
+    var curr_data = $.extend(true,{},ChartOptions['data']);
+    if(available_filter.length==0)
     {
-      filter = document.getElementById('filter_'+slot);
-      if(filter.value != 2 && filter.value != 4)
+      console.log('Resetting');
+    }
+    else
+    {
+      var filter = null;
+      var filter_val = [null,null];
+      for(const slot of available_filter)
       {
-        filter_val[0] = document.getElementById(`filter_input_${slot}_0`).value;
-        filter_val[1] = document.getElementById(`filter_input_${slot}_1`).value;
-        curr_data = numerical_filter(curr_data,filter.value,filter_val[0],filter_val[1]);
-      }
-      else
-      {
-        filter_val[0] = document.getElementById(`filter_input_${slot}_0`).value;
-        curr_data = lexicographic_filter(curr,filter.value,filter_val[0]);
+        filter = document.getElementById('filter_'+slot);
+        if(filter.value != 2 && filter.value != 4)
+        {
+          filter_val[0] = document.getElementById(`filter_input_${slot}_0`).value;
+          filter_val[1] = document.getElementById(`filter_input_${slot}_1`).value;
+          curr_data = numerical_filter(curr_data,filter.value,filter_val[0],filter_val[1]);
+        }
+        else
+        {
+          filter_val[0] = document.getElementById(`filter_input_${slot}_0`).value;
+          curr_data = lexicographic_filter(curr_data,filter.value,filter_val[0]);
+        }
       }
     }
-
-    option['series']['data']=curr_data;
+    for(const i of Object.keys(curr_data))
+    {
+      option['series'][i]['data'] = curr_data[i];
+    }
     option && Chart.setOption(option);
     return;
   });
