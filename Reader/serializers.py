a64eaaf05bb4800c7cd345acb5e3f6ec15c8fc33
Maarten Mäcking <maarten.maecking@studium.uni-hamburg.de>
2023-01-24 22:42:14 +0100

    Create media/exported_data/ if not exists, remove Reader/ and data/

--- Reader/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Substance
-
-
-class SubstanceSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Substance
-        fields = ('PubChem_Name', 'PubChem_Alias', 'PubChem_CID', 'PubChem_Mass', 'PubChem_Formula')
--- Reader/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .serializers import SubstanceSerializer
-from .models import Substance
-
-
-class SubstanceViewSet(viewsets.ModelViewSet):
-    queryset = Substance.objects.all().order_by('PubChem_CID')
-    serializer_class = SubstanceSerializer
-    http_method_names = ['get', 'head']
