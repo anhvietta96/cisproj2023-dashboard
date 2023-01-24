3e1b4220d8cc0f355b1362e72780bfee76fe099a
Maarten Mäcking <maarten.maecking@studium.uni-hamburg.de>
2023-01-24 22:58:21 +0100

    Remove unused code in charts/

--- chart/templatetags/tags.py
+++ chart/templatetags/tags.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='decrement')
 def decrement(input):
-    return input-1
+    return input - 1
--- chart/urls.py
+++ chart/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path,include
-from django.contrib import admin
-from .views import ChartOptions,ChartResult,Export_CSV,Export_SDF
+from django.urls import path
+from .views import ChartOptions, ChartResult, Export_CSV, Export_SDF
 
 urlpatterns = [
     path('', ChartOptions, name='chart_options'),
-    path('result/',ChartResult,name='chart_result'),
-    path('result/csv',Export_CSV,name='csv_download'),
-    path('result/sdf',Export_SDF,name='sdf_download'),
+    path('result/', ChartResult, name='chart_result'),
+    path('result/csv', Export_CSV, name='csv_download'),
+    path('result/sdf', Export_SDF, name='sdf_download'),
 ]
