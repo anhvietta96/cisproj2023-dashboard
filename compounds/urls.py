e8809074e1013544ceb1485d7496596ae5b9d6c1
Maarten <maarten.maecking@studium.uni-hamburg.de>
2023-02-01 18:51:15 +0100

    Small fix for compounds/urls

--- compounds/urls.py
+++ compounds/urls.py
@@ -1,18 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib import admin
 from . import views
-from rest_framework import routers
 
 app_name = 'compounds'
-
-router = routers.DefaultRouter()
-router.register(r"compounds", views.CompoundViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_compound_view, name='compounds'),
-    path('search/', views.SearchResultsView.as_view(), name='search_url'),
-    path('filter/', views.filter_compound_view, name='filter_url'),   #doesnt quite work yet
-    path('<str:inchi_key>', views.molecule_single_view),
-    path('data/', include(router.urls), name='data'),
+    path('filter/', views.filter_compound_view, name='filter_url'),
+    path('<str:inchi_key>', views.molecule_single_view)
 ]
